42666117be49cf67a809f5f44fbb195f082b6137
Carlos Gabriel Leite Barros <carlosgabrieldev@gmail.com>
2021-03-12 12:44:51 -0300

    front-end desenvolvido
    todo o layout do projeto foi desenvolvido, incluindo responsividade

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Crowdfunding-project
- Um projeto de um site de financiamento colaborativo para um projeto ficticio com escolha de planos e valores. Este projeto é um desafio css e javascript
+<h1>
+    Projeto Crowfunding
+</h1>
+
+## 📄Sobre
+
+O projeto Crowfunding é um site ficticio de financiamento coletivo com todas as funcionalidades de seleção de planos de promessa e contagem de dinheiro arrecadado. 
+
+O site é totalmente responsivo se adequando as telas dos variados dispositivos. para isso utilizei o flexbox, css grid e JavaScript. Toda interação tem uma micro-interação feita com javaScript e Jquery.
+
+Este foi um desafio css tirado do site Front-end Mentor. Considero que esse projeto foi muito gratificante de desenvolver e com isso pude aprimorar diversas das minhas habilidades.
+
+## 🚀tecnologias utilizadas
+
+As seguintes tecnologias foram utilizadas:
+
+- HTML e CSS
+- JavaScript
+- Jquery
+
+---
+
+🚧 JavaScript em desenvolvimento 🚧
+
+---
+
+Desenvolvido por Carlos Gabriel
---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(()=>{ //quando lê a pagina
+    $('.bg-dark').on('click', ()=>{
+        let ativo = $('.hamburguerAtivo').length
+
+        if(ativo == 1) {
+            $('.ul-mobile').removeClass('open')
+            $('.ul-mobile').addClass('close')
+            setTimeout(()=>{
+                $('.hamburguer').removeClass('hamburguerAtivo')
+                $('.bg-dark').fadeOut()
+                $('.ul-mobile').fadeOut()
+                setTimeout(()=>{
+                    $('.ul-mobile').removeClass('close')
+                }, 400)
+            }, 200)
+        }
+
+        $('.modal').fadeOut()
+        $('.bg-dark').fadeOut()
+
+    })
+
+    $('.close-modal').click(()=>{
+        $('.modal').fadeOut()
+        $('.bg-dark').fadeOut()
+    })
+
+    $('.menu-hamburguer-wrap').on('click', ()=>{ //dispara após o clique no menu-hamburguer
+
+        let ativo = $('.hamburguerAtivo').length
+
+        if(ativo != 1) {
+            $('.hamburguer').addClass('hamburguerAtivo')
+            $('.bg-dark').fadeIn()
+            setTimeout(()=>{
+                $('.ul-mobile').fadeIn()
+                $('.ul-mobile').addClass('open')
+            }, 200)
+        } else {
+            $('.ul-mobile').removeClass('open')
+            $('.ul-mobile').addClass('close')
+            setTimeout(()=>{
+                $('.hamburguer').removeClass('hamburguerAtivo')
+                $('.bg-dark').fadeOut()
+                $('.ul-mobile').fadeOut()
+                setTimeout(()=>{
+                    $('.ul-mobile').removeClass('close')
+                }, 400)
+            }, 200)
+        }
+
+    }) //fim do disparo no clique no menu hamburguer
+
+    $('.btn-marked, .btn-marked-mobile').on('click', ()=>{ //disparo botão de marcação
+        let ativo = $('.marcado').length
+
+        if(ativo != 2) {
+            $('.btn-marked').addClass('marcado')
+            $('.btn-marked-mobile').addClass('marcado')
+        } else {
+            $('.btn-marked').removeClass('marcado')
+            $('.btn-marked-mobile').removeClass('marcado')
+        }
+    })
+
+
+    $('.btn').on('click', ()=>{
+        
+        $('.modal').fadeIn()
+        $('.bg-dark').fadeIn()
+    })
+    
+
+})
